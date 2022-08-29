f1663e1ba1fda89635ed6d37e5084c753bb1e340
FernandoDalai <78560278+FernandoDalai@users.noreply.github.com>
2022-08-29 13:25:27 -0500

    .

--- Sum.c
+++ Sum.c
@@ -1,16 +1,40 @@
-#include <stdio.h>
+#include <stdio.h> //\n
 
 #define Suma(n) n*(n+1)/2
 #define S2(n) (n*(n+1)*(2*n+1))/6
-#define S3(n) (n*(n+1)/2)*(n*(n+1)/2)
+#define S3(n) Suma(n) * Suma(n)
+#define S3p(n) 2*(n*(n+1) * n*(n+1))
+#define S3p2(n) 8*Suma(n)*Suma(n)
 
 int main(){
 
-    int n;
+    int n, s;
 
-    printf("Ingrese el n-esimo término: ");
+    printf("Este programa suma enteros. Ingrese el n-esimo término: \n");
 
-    scanf("%i", &n);
+    scanf("%d", &n);
 
+    s = Suma(n);
+
+    printf("Suma de los primeros %d numeros es: %d \n", n ,s);
+
+    s = S2(n);
+
+    printf("Suma de los cuadrados de los primeros %d numeros es: %d \n", n ,s);
+    
+    s = S3(n);
+
+    printf("Suma de los cubos de los primeros %d numeros es: %d \n", n ,s);
+    
+    s = S3p(n);
+
+    printf("Suma de los cubos de los primeros %d numeros pares es: %d \n", n ,s);
+    
+    s = S3p2(n);
+
+    printf("Suma de los cubos de los primeros %d numeros pares es: %d \n", n ,s);
+
+    return 0;
+    
 
 }
--- Sum.c++
+++ Sum.c++
@@ -4,15 +4,38 @@ using namespace std;
 
 #define Suma(n) n*(n+1)/2
 #define S2(n) (n*(n+1)*(2*n+1))/6
-#define S3(n) (n*(n+1)/2)*(n*(n+1)/2) 
+#define S3(n) Suma(n) * Suma(n)
+#define S3p(n) 2*(n*(n+1) * n*(n+1))
+#define S3p2(n) 8*Suma(n)*Suma(n)
+
 
 int main(){
 
-    int n;
+    int n, s;
 
-    cout<<"Ingrese el n-esimo término: ";
+    cout<<"Este programa suma enteros. Ingrese el n-esimo término: \n";
 
     cin>>n;
+
+    s = Suma(n);
+
+    cout<<"Suma de los primeros"<<n<<"numeros es:"<<s<<endl;
+
+    s = S2(n);
+
+    cout<<"Suma de los cuadrados de los primeros"<<n<<"numeros es:"<<s<<endl;
+
+    s = S3(n);
+
+    cout<<"Suma de los cubos de los primeros"<<n<<"numeros es:"<<s<<endl;
+
+    s = S3p(n);
+
+    cout<<"Suma de los cubos de los primeros"<<n<<"numeros pares es:"<<s<<endl;
+
+    s = S3p2(n);
+
+    cout<<"Suma de los cubos de los primeros"<<n<<"numeros pares es:"<<s<<endl;
 
 
 
