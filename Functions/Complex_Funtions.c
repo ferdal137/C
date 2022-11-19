4f7e7e030f122ff43492c2007fd2ba6a4217558d
Fernando Dalai Aguilar Sanchez <ferdalaixd@gmail.com>
2022-11-18 20:46:32 -0600

    .

--- Functions/Complex_Funtions.c
+++ Functions/Complex_Funtions.c
@@ -1,9 +1,6 @@
 # include <stdio.h>
-# include <stlib.h>
+# include <stdlib.h>
 # include <math.h>
-
-#define COMP_COMPLEJOS(X, Y) \
-    (((X).re == (Y).re) && ((X).im == (Y).im))
 
 typedef struct complejos{
     double re;
@@ -14,22 +11,24 @@ COMP leerC();
 
 int main(int argc, char *argv){
 
-    COMP complejos uI = {0.0, 1.0}, uR = {im:0.0, re:1.0}, z;
+    COMP A;
+
+    A = leerC();
+    printf("El numero complejo que digito fue: %lg %+lg i\n", A.re, A.im);
+
+
+    return 0;
+
+}
+
+
+COMP leerC(){
+
+    COMP z;
 
     printf("Por leer un numero complejo.\nDigite la parte real:");
     scanf("%lg", &(z.re));
     printf("Digite la parte imaginaria:");
     scanf("%lg", &(z.im));
-
-    if (COMP_COMPLEJOS(uI, z))
-        printf("Digito la unidad imaginaria\n");
-    else if (COMP_COMPLEJOS(uR, z))
-        printf("Digito la unidad real\n");
-    else
-        printf("El numero complejo que digito fue: %lg %+lg i\n", z.re, z.im);
-
-
-
-    return 0;
 
 }
