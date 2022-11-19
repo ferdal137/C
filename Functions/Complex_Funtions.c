793b3cbbe27125102af46a6b931cc4c8829ed31d
Fernando Dalai Aguilar Sanchez <ferdalaixd@gmail.com>
2022-11-18 20:56:53 -0600

    .

--- Functions/Complex_Funtions.c
+++ Functions/Complex_Funtions.c
@@ -8,6 +8,7 @@ typedef struct complejos{
 }COMP;
 
 COMP leerC();
+void raices(COMP z);
 
 int main(int argc, char *argv){
 
@@ -15,6 +16,11 @@ int main(int argc, char *argv){
 
     A = leerC();
     printf("El numero complejo que digito fue: %lg %+lg i\n", A.re, A.im);
+
+    raices(A);
+
+
+
 
 
     return 0;
@@ -33,4 +39,47 @@ COMP leerC(){
 
     return z;
 
+}
+
+void raices(COMP z){
+
+    double a,b,r1,r2,r;
+
+    a = z.re;
+    b = z.im;
+    if(b==0.0){
+        if(a>0.0){
+            r1=sqrt(a);
+            printf("La soluciones son r1=%lg y r2=%lg",r1,-r1);
+        }else if(a<0.0){
+            r1=sqrt(-a);
+            if(r1==1.0) printf("Las soluciones son i y -i");
+            else printf("La soluciones son r1=%lgi y r2=%lgi ",r1,-r1);
+        }else{
+            printf("0.0 es una solución doble");
+        }
+
+    if(b>0.0){
+        r=sqrt(a*a+b*b);
+        r1=sqrt((a+r)/2.0);
+        r2=sqrt((-a+r)/2.0);
+        printf("La soluciones son r1=%lg",r1);
+        if(r2==1.0){
+            printf("+i y r2=-%lg-i",r1);
+        }else {
+            printf("+i%lg y r2=-%lg-i%lg",r2,r1,r2);
+        }
+    }
+
+    }
+    
+    if (b<0.0){
+    	printf("Las soluciones son: r1=%lg",r1);
+    	if(r2==1.0){
+    		printf("-i y r2=-%lg+i",r1);
+		}else{
+			printf("-i%lg y r2=-%lg+i%lg",r2,r1,r2);
+		}
+	}
+    
 }
