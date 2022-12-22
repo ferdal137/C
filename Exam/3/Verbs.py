0f2c24cea0b925a78ecc16d797735c2ccf3a6eff
Fernando Dalai Aguilar Sanchez <ferdalaixd@gmail.com>
2022-12-22 12:26:30 -0600

    .

--- Exam/3/Verbs.py
+++ Exam/3/Verbs.py
@@ -56,8 +56,6 @@ int main(int argc, char *argv[])
   lbuffer = strlen(buffer);
   //ter = buffer[1]; // + buffer[lbuffer];
   ter = buffer[lbuffer-2];
-  printf("%c",ter);
-  printf("\n");
                                
   buffer[lbuffer-2]= '\0';    
 
@@ -65,15 +63,15 @@ int main(int argc, char *argv[])
   	
   	switch(ter){
         case 'a':
-            conjugacionRegular(miArchivo, buffer);
+            conjugacionRegularAR(miArchivo, buffer);
             break;
 
         case 'e':
-            conjugacionRegular(miArchivo, buffer);
+            conjugacionRegularER(miArchivo, buffer);
             break;
 
         case 'i':
-            conjugacionRegular(miArchivo, buffer);
+            conjugacionRegularIR(miArchivo, buffer);
             break;
 
     }
@@ -89,7 +87,37 @@ int main(int argc, char *argv[])
   return 0;
 }          
 
-int conjugacionRegular(FILE *archivo, char raizVerbo[])                                                                           
+int conjugacionRegularAR(FILE *archivo, char raizVerbo[])                                                                           
+{                                                                               
+  int  lpronombre=8, ldesidencia=4,tiempo, lverbo, persona, centrado, titulo;           
+  char formato[1024];                                                           
+  
+  lverbo = strlen(raizVerbo)+2;
+
+  for(tiempo=0;tiempo<=4;tiempo++)
+   {
+  		titulo = strlen(ModoIndicativoSimple[tiempo])/2;
+	    centrado = (lverbo+lpronombre+ldesidencia+1)/2;
+	    if (centrado<=titulo) centrado=0;
+  			else centrado -= titulo;         
+		sprintf(formato, "%%%ds%%s%%%ds", centrado, centrado+2);
+	    fprintf(archivo, formato, " ", ModoIndicativoSimple[tiempo]," ");
+	    
+   }
+	  fprintf(archivo,"\n\n ");
+  for (persona=0; persona<TotalPronombres; persona++)
+   {
+		for(tiempo=persona;tiempo<5*TotalPronombres;tiempo+=6) 
+	  		fprintf(archivo, "%8s %s%s", Pronombres[persona], 
+    		        raizVerbo, DesidenciaSimpleAR[tiempo]); 
+        fprintf(archivo, "\n");
+   }                                                           
+  fprintf(archivo, "\n");
+
+  return 0;  
+}
+
+int conjugacionRegularER(FILE *archivo, char raizVerbo[])                                                                           
 {                                                                               
   int  lpronombre=8, ldesidencia=4,tiempo, lverbo, persona, centrado, titulo;           
   char formato[1024];                                                           
@@ -112,6 +140,36 @@ int conjugacionRegular(FILE *archivo, char raizVerbo[])
 		for(tiempo=persona;tiempo<5*TotalPronombres;tiempo+=6) 
 	  		fprintf(archivo, "%8s %s%s", Pronombres[persona], 
     		        raizVerbo, DesidenciaSimpleER[tiempo]); 
+        fprintf(archivo, "\n");
+   }                                                           
+  fprintf(archivo, "\n");
+
+  return 0;  
+}
+
+int conjugacionRegularIR(FILE *archivo, char raizVerbo[])                                                                           
+{                                                                               
+  int  lpronombre=8, ldesidencia=4,tiempo, lverbo, persona, centrado, titulo;           
+  char formato[1024];                                                           
+  
+  lverbo = strlen(raizVerbo)+2;
+
+  for(tiempo=0;tiempo<=4;tiempo++)
+   {
+  		titulo = strlen(ModoIndicativoSimple[tiempo])/2;
+	    centrado = (lverbo+lpronombre+ldesidencia+1)/2;
+	    if (centrado<=titulo) centrado=0;
+  			else centrado -= titulo;         
+		sprintf(formato, "%%%ds%%s%%%ds", centrado, centrado+2);
+	    fprintf(archivo, formato, " ", ModoIndicativoSimple[tiempo]," ");
+	    
+   }
+	  fprintf(archivo,"\n\n ");
+  for (persona=0; persona<TotalPronombres; persona++)
+   {
+		for(tiempo=persona;tiempo<5*TotalPronombres;tiempo+=6) 
+	  		fprintf(archivo, "%8s %s%s", Pronombres[persona], 
+    		        raizVerbo, DesidenciaSimpleIR[tiempo]); 
         fprintf(archivo, "\n");
    }                                                           
   fprintf(archivo, "\n");
