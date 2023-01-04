2e9f209db8a9be3f3dbc625b24e047ac16957cbf
Fernando Dalai Aguilar Sanchez <ferdalaixd@gmail.com>
2023-01-03 20:40:48 -0600

    .

--- Acentos.c
+++ Acentos.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <locale.h>
 
+//Implementation of the locale library to can show accents in the console 
+
 int main(int argc, char *argv[]) {
 	setlocale(LC_ALL,"");
 	printf("Gracias a locale.h puedo poner palabras acentuadas como... México :')");
