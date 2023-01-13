2a8f5d6a578fe6ecaea6695cc07d9967eeb7c431
Fernando Dalai Aguilar Sanchez <ferdalaixd@gmail.com>
2023-01-12 21:22:58 -0600

    .

--- Exam/3/Verbs.c
+++ Exam/3/Verbs.c
@@ -21,7 +21,11 @@ enBasePronombres DesidenciaSimpleAR = {"o       ","as      ","a       ","amos   
 									   "é       ","aste    ","ó       ","amos    ","aron    ","aron   ",
 									   "aré      ","arás   ","ará    ","aremos    ","aran    ","arán    ",
 									   "aba      ","abas   ","aba     ","ábamos    ","aban    ","aban    ",
-									   "aría      ","arías   ","antaría     ","ariamos    ","arian    ","arian    "};   
+									   "aría      ","arías   ","antaría     ","ariamos    ","arian    ","arian    "};
+									   
+enBasePronombres DesidenciaSubjAR =   {"e       ","es      ","e       ","emos    ","en     ","en      ",
+									   "ara      ","aras   ","ara    ","áramos    ","aran    ","aran    ",
+									   "are      ","ares   ","are    ","áremos    ","aren    ","aren    "}; 									   
 
 enBasePronombres DesidenciaSimpleER = {"o       ","es      ","e       ","emos    ","en     ","en      ",
 									   "i       ","iste    ","ió       ","imos    ","ieron    ","ieron   ",
@@ -40,6 +44,10 @@ enBasePronombres DesidenciaCompuesta = {"he ","has ","ha ","hemos ","han ","han 
 									   "habré ","habrás ","habrá ","habremos ","habrán ","habrán ",
 									   "había ","habías ","había ","habíamos ","habían ","habían ",
 									   "habría ","habrías ","habría ","habríamos ","habrían ","habrían "};
+									   
+enBasePronombres DesidenciaCompuestaSubj = {"haya ","hayas ","haya ","hayemos ","hayan ","hayan ",
+									   "hubiera ","hubieras ","hubiera ","hubieramos ","hubieran ","hubieran ",
+									   "hubiere ","hubieres ","hubiere ","hubiéremos ","hubieren ","hubieren "};
 
 enBasePronombres Oler = {"huelo      ","hueles      ","huele       ","oelmos    ","huelen     ","huelen      ",
 						"olí      ","oliste   ","olío     ","olimos    ","olieron    ","olieron    ",
@@ -52,7 +60,9 @@ enBasePronombres Pronombres = {"yo", "tú", "él", "nosotros", "ustedes", "ellos
 int conjugacionRegularAR(FILE *archivo, char raizVerbo[]);
 int conjugacionRegularER(FILE *archivo, char raizVerbo[]);
 int conjugacionRegularIR(FILE *archivo, char raizVerbo[]);
-int conjugacionCompuesta(FILE *archivo, char raizVerbo[], char ter_comp[]); 
+int conjugacionCompuesta(FILE *archivo, char raizVerbo[], char ter_comp[]);
+int conjugacionCompuestaSubj(FILE *archivo, char raizVerbo[], char ter_comp[]);
+int conjugacionSubjuntivaAR(FILE *archivo, char raizVerbo[]);
 int conjugacionIrregular(FILE *archivo, char raizVerbo[]);                                                                     
 
 int main(int argc, char *argv[])
@@ -104,6 +114,8 @@ int main(int argc, char *argv[])
 	        case 'a':
 	            conjugacionRegularAR(miArchivo, buffer);
 	            conjugacionCompuesta(miArchivo,buffer,ter_comp);
+	            conjugacionSubjuntivaAR(miArchivo, buffer);
+	            conjugacionCompuestaSubj(miArchivo,buffer,ter_comp);
 	            break;
 	
 	        case 'e':
@@ -159,6 +171,36 @@ int conjugacionRegularAR(FILE *archivo, char raizVerbo[])
 		for(tiempo=persona;tiempo<5*TotalPronombres;tiempo+=6) 
 	  		fprintf(archivo, "%8s %s%s", Pronombres[persona], 
     		        raizVerbo, DesidenciaSimpleAR[tiempo]); 
+        fprintf(archivo, "\n");
+   }                                                           
+  fprintf(archivo, "\n");
+
+  return 0;  
+}
+
+int conjugacionSubjuntivaAR(FILE *archivo, char raizVerbo[])                                                                           
+{                                                                               
+  int  lpronombre=8, ldesidencia=4,tiempo, lverbo, persona, centrado, titulo;           
+  char formato[1024];                                                           
+  
+  lverbo = strlen(raizVerbo)+2;
+
+  for(tiempo=0;tiempo<=2;tiempo++)
+   {
+  		titulo = strlen(ModoIndicativoSimple[tiempo])/2;
+	    centrado = (lverbo+lpronombre+ldesidencia+1)/2;
+	    if (centrado<=titulo) centrado=0;
+  			else centrado -= titulo;         
+		sprintf(formato, "%%%ds%%s%%%ds", centrado, centrado+2);
+	    fprintf(archivo, formato, " ", ModoIndicativoSimple[tiempo]," ");
+	    
+   }
+	  fprintf(archivo,"\n\n ");
+  for (persona=0; persona<TotalPronombres; persona++)
+   {
+		for(tiempo=persona;tiempo<3*TotalPronombres;tiempo+=6) 
+	  		fprintf(archivo, "%8s %s%s", Pronombres[persona], 
+    		        raizVerbo, DesidenciaSubjAR[tiempo]); 
         fprintf(archivo, "\n");
    }                                                           
   fprintf(archivo, "\n");
@@ -255,6 +297,35 @@ int conjugacionCompuesta(FILE *archivo, char raizVerbo[],  char ter_comp[])
   return 0;  
 }
 
+int conjugacionCompuestaSubj(FILE *archivo, char raizVerbo[],  char ter_comp[])                                                                           
+{                                                                               
+  int  lpronombre=8, ldesidencia=4,tiempo, lverbo, persona, centrado, titulo;           
+  char formato[1024];                                                           
+  
+  lverbo = strlen(raizVerbo)+2;
+
+  for(tiempo=0;tiempo<=2;tiempo++)   {
+  		titulo = strlen(ModoIndicativoSimple[tiempo])/2;
+	    centrado = (lverbo+lpronombre+ldesidencia+1)/2;
+	    if (centrado<=titulo) centrado=0;
+  			else centrado -= titulo;         
+		sprintf(formato, "%%%ds%%s%%%ds", centrado, centrado+2);
+	    fprintf(archivo, formato, " ", ModoIndicativoSimple[tiempo]," ");
+	    
+   }
+	  fprintf(archivo,"\n\n ");
+  for (persona=0; persona<TotalPronombres; persona++)
+   {
+		for(tiempo=persona;tiempo<3*TotalPronombres;tiempo+=6) 
+	  		fprintf(archivo, "%8s %s%s%s        ", Pronombres[persona], 
+    		        DesidenciaCompuestaSubj[tiempo], raizVerbo, ter_comp);
+        fprintf(archivo, "\n");
+   }                                                           
+  fprintf(archivo, "\n");
+
+  return 0;  
+}
+
 int conjugacionIrregular(FILE *archivo, char raizVerbo[])                                                                           
 {                                                                               
   int  lpronombre=8, ldesidencia=4,tiempo, lverbo, persona, centrado, titulo;           
@@ -284,4 +355,3 @@ int conjugacionIrregular(FILE *archivo, char raizVerbo[])
 
   return 0;  
 }
-
