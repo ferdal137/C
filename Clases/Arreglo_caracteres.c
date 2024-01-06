6bae0dcaf414e06bd0ee5861a02ce6eda2c711ef
Fernando Dalai Aguilar Sanchez <ferdalaixd@gmail.com>
2024-01-06 08:46:28 -0600

    Create Arreglo_caracteres.c

---
+++ Clases/Arreglo_caracteres.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Declaración e inicialización de un arreglo de caracteres
+    char saludo[] = "Hola";
+
+    // Imprimir la cadena
+    printf("Saludo: %s\n", saludo);
+
+    return 0;
+}
