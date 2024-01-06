06fdbc81967dc38dc51ed39d05006565ffce0afa
Fernando Dalai Aguilar Sanchez <ferdalaixd@gmail.com>
2024-01-06 08:45:53 -0600

    Create Arreglos.c

---
+++ Clases/Arreglos.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // Declaración de un arreglo de enteros
+    int numeros[5];
+
+    // Inicialización de elementos
+    numeros[0] = 10;
+    numeros[1] = 20;
+    numeros[2] = 30;
+    numeros[3] = 40;
+    numeros[4] = 50;
+
+    // Imprimir el tercer elemento
+    printf("El tercer elemento es: %d\n", numeros[2]);
+
+    return 0;
+}
