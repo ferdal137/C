00f67c40218379ea1b24cb0f41ae2930fb001344
Fernando Dalai Aguilar Sanchez <ferdalaixd@gmail.com>
2024-01-06 08:38:43 -0600

    Create Funcion_suma.c

---
+++ Clases/Funcion_suma.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Definición de la función
+int suma(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int resultado = suma(5, 3); // Llamada a la función
+    printf("La suma es: %d\n", resultado);
+    return 0;
+}
